8646ecf616e049422e4fe119dc6325ca584d9f09
Kshitiz Gajurel <kshitiz.purbct042@gmail.com>
2022-04-23 06:59:24 +0545

    First program added

---
+++ Firstgitprogram.py
@@ -0,0 +1,18 @@
+message = input('ENter the message:')
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+key = 5
+encrypt =''
+decrypt = ''
+for i in message:
+    position = alphabet.find(i)
+    newposition = (position+key)%26
+    encrypt += alphabet[newposition]
+
+print(encrypt)
+      
+for i in encrypt:
+    pos = alphabet.find(i)
+    newpos = (pos-key)%26
+    decrypt += alphabet[newpos]
+
+print(decrypt)
